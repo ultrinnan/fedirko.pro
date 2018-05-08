2316aa303763035d6dedf6a872a6121a3e651a0a
Serhii Fedirko <serhii.fedirko@infopulse.com>
2018-05-08 16:31:22 +0300

    GTM;

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -104,6 +104,13 @@ $topItems = [
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KDGDVMX');</script>
+    <!-- End Google Tag Manager -->
     <link rel="icon" type="image/png" href="/favicon.png">
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -112,6 +119,10 @@ $topItems = [
     <?php $this->head() ?>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KDGDVMX"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php $this->beginBody() ?>
 
 <header>
