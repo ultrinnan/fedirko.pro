ac6ebc7a7b9fbd51edcd6540762e6813465777a9
Serhii Fedirko <serhii.fedirko@infopulse.com>
2018-06-14 11:16:13 +0300

    google fonts import; gulp setup; styles to sass wip;

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,13 +12,27 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/main.css',
+        'css/main.min.css'
     ];
     public $js = [
-        'js/main.js'
+        'js/main.min.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapPluginAsset',
     ];
+
+    //redefine properties for dev environment and connect unminified scripts
+    public function init()
+    {
+        if (YII_ENV_DEV){
+            $this->css = [
+                'css2/main.css'
+            ];
+            $this->js = [
+                'js/main.js'
+            ];
+        }
+        return true;
+    }
 }
